f68003973568a7f11f1b10f5cd7435c2f6788e8e
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-05-20 16:49:01 -0400

    Rellena paginas
    - Descargas
    - Nosotros

--- src/Pages/Descargas.js
+++ src/Pages/Descargas.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React from "react";
 
 function Descargas() {
-    return(
-        <>
-            <main>
+  return (
+    <>
+      <main>
+      <br/>
 
-            </main>
-        </>
-    )
+        <div className="container my-5">
+          <h3 className="text-center">Descarga</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            ex eaque blanditiis aliquam! Beatae aperiam voluptas totam
+            perferendis ab harum incidunt quo aspernatur labore nostrum,
+            suscipit quis corporis rem commodi.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+            omnis expedita quo ratione error minus id laudantium, placeat dolore
+            optio exercitationem provident sequi. Maxime nisi dicta est,
+            voluptas officia optio.
+          </p>
+
+          <h3 className="text-center">Tutorial</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            corrupti harum dignissimos tenetur voluptatibus animi quibusdam
+            quidem laboriosam facilis, eum dolorum nobis quos iusto cum
+            repellat? Ipsum excepturi magnam quod?
+          </p>
+        </div>
+
+        <div class="col-lg-8 offset-lg-2 text-center">
+          <div class="d-inline-block v-align-top">
+            <a
+              class="btn btn-primary"
+              id="download-button"
+              href="https://anydesk.com/n"
+            >
+              <p class="mb-0 d-inline-block">Download Now</p>
+            </a>
+            <div class="mt-2"></div>
+          </div>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Descargas;
