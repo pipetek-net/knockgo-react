0717b1fce2708ca25fd91505ab64d1522d9dbe70
joaquinrfs <joaquinrfs@tutanota.com>
2022-07-01 03:44:46 -0400

    Rellena pagina Descargas

--- src/Pages/Descargas.jsx
+++ src/Pages/Descargas.jsx
@@ -7,30 +7,33 @@ function Descargas() {
         <div className="container my-5">
             <div className="row">
                 <div className="col-8">
-                    <div className="row">
-                        <h2>Servicio Remoto</h2>
-                        <p>Nuestro servicio se basa en reparación remota (por internet) de dispositivos. Para esto es necesario que el cliente descargue el software que utilizamos en Knock&Go.</p>
+                    <div className="row mb-4">
+                        <h1>Servicio Remoto</h1>
+                        <p>Nuestro servicio se basa en reparación remota (por internet) de dispositivos. Para esto es necesario que el cliente descargue el programa que utilizamos en Knock&Go.</p>
+                        <p>Si necesitas ayuda instalando el programa, puedes guiarte con una de las guías a la derecha o seguir el siguiente video tutorial.</p>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/0Qd7dwmZpE4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="mt-1"></iframe>
                     </div>
                     <div className="row">
-                        <h2>Descargas</h2>
-                        <div className="d-flex justify-content-between">
+                        <h1>Descargas</h1>
+                        <p>A continuación podrá descargar el programa adecuado para el dispositivo que esta teniendo problemas.</p>
+                        <div className="d-flex justify-content-between mt-2">
                             <div className="flex-grow-1 d-flex flex-column align-items-center">
-                                <h4 className="text-center"><i className="fa-brands fa-windows fst-normal"></i> Windows</h4>
+                                <h4 className="text-center mb-3"><i className="fa-brands fa-windows fst-normal"></i> Windows</h4>
                                 <a href="https://download.anydesk.com/AnyDesk.exe" className="btn btn-primary w-50" download>Descargar</a>
                             </div>
                             <div className="flex-grow-1 d-flex flex-column align-items-center border-start border-end">
-                                <h4 className="text-center"><i className="fa-brands fa-apple fst-normal"></i> MacOS</h4>
+                                <h4 className="text-center mb-3"><i className="fa-brands fa-apple fst-normal"></i> MacOS</h4>
                                 <a href="https://download.anydesk.com/anydesk.dmg" className="btn btn-primary w-50" download>Descargar</a>
                             </div>
                             <div className="flex-grow-1 d-flex flex-column align-items-center">
-                                <h4 className="text-center"><i className="fa-brands fa-linux fst-normal"></i> Linux</h4>
+                                <h4 className="text-center mb-3"><i className="fa-brands fa-linux fst-normal"></i> Linux</h4>
                                 <a href="https://download.anydesk.com/linux/anydesk_6.2.0-1_amd64.deb" className="btn btn-primary w-50" download>Descargar</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-4 border-start">
-                    <div className="ms-3">
+                    <div className="ms-1">
                         <h2>Links</h2>
                         <ul className="list-unstyled">
                             <li><a href="#">Instalación Windows.pdf</a></li>
