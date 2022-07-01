0c65e431e9450dd2de7c63a62d2fa2b9ccc726e4
joaquinrfs <joaquinrfs@tutanota.com>
2022-07-01 04:36:14 -0400

    Agrega Footer basico

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
 import Header from './Layout/Header';
+import Footer from './Layout/Footer';
 
 const Layout = () => {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     )
 }
---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-white w-100 py-2">
+            <div className="w-100 d-flex justify-content-center">
+                Copyright © 2022 PipeTek, todos los derechos reservados.
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
